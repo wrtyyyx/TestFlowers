2595ffc3543f853ec8b466f8ca3cd632d9d5435d
wrtyyyx <126886326+wrtyyyx@users.noreply.github.com>
2025-05-21 23:02:25 +0300

    Change render component

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -5,14 +5,13 @@ import RollingGallery from '../../blocks/Components/RollingGallery/RollingGaller
 
 const Promo = () => {
     const { data: flowers = [], isLoading } = useGetProductsQuery();
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+
     return (
         <>
             <div className={'promo'}>
                 <h1 className={'promo_text'}>BIG SALE 20%</h1>
-                <RollingGallery images={flowers} />
+                {isLoading ? (<div>Loading...</div>) : <RollingGallery images={flowers} /> }
+
             </div>
         </>
     );
